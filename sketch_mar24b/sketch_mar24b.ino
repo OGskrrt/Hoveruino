41617856e31467d68011f30551d45d342fe98f46
Adem Alperen Arda <alperen.arda.adem22@gmail.com>
2024-03-26 19:30:24 +0300

    Added Hover, potentiometer, 7 segments
    Added Hover, potantiometer, 7 segments but potentiometer doesnt work.

--- sketch_mar24b/sketch_mar24b.ino
+++ sketch_mar24b/sketch_mar24b.ino
@@ -1,18 +1,21 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 128 // OLED ekran genişliği
 #define SCREEN_HEIGHT 64 // OLED ekran yüksekliği
 #define OLED_ADDR   0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_ADDR);
 
+int potPin = A3;
+
 int arrX[8][4];
 int arrY[8][4];
 int rectWidth = 8;
 int rectHeight = 4;
 
-int hoverWidth = 16;
+int hoverWidth = 20;
 int hoverHeight = 2;
 int hoverX;
 int hoverY;
@@ -23,6 +26,7 @@ int ballWidth = 2;
 int ballHeight = 2;
 
 void setup() {
+  pinMode(potPin, INPUT);
   Serial.begin(9600);
   if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
     Serial.println(F("SSD1306 başlatma hatası!"));
@@ -37,6 +41,7 @@ void setup() {
        arrY[i][j] = i + 1;  
     }
   }
+
   /*
   hareketsiz bir bileşen çizilecekse kullanılacak, şimdilik gerek yok
   display.clearDisplay();
@@ -51,11 +56,15 @@ void loop() {
   {
     for(int j = 0;j < 8; j++)
     {
-      //display.drawRect((arrX[i][j] * (rectWidth + 1)) + 18, (arrY[i][j] * (rectHeight + 1)), rectWidth, rectHeight, SSD1306_WHITE); kank buna gerek yok ama silmedim göresin diye
-      display.fillRect((arrX[i][j] * (rectWidth + 1)) + 18, (arrY[i][j] * (rectHeight + 1)), rectWidth, rectHeight, SSD1306_WHITE); //burda 18 sağ sol boşluğu sağlıyor(margin). 1 ise kareler arası boşlukları. okuduktan sonra silersin.
-      //tam 8 * 4 olmuyor bakarsın
+      display.fillRect((arrX[i][j] * (rectWidth + 1)) + 18, (arrY[i][j] * (rectHeight + 1)), rectWidth, rectHeight, SSD1306_WHITE);
     }
   }
-  
+
+  //hover çizimi
+  int potValue = analogRead(potPin);
+  float x = (float)potValue / 1023.0 * (SCREEN_WIDTH - hoverWidth); // Potansiyometre değerini x ekseni değerine dönüştüren formül(bunda sorun yok)
+  int x2 = round(x);
+  display.fillRect(x2, 62, hoverWidth, hoverHeight, SSD1306_WHITE);
+
   display.display();
 }
